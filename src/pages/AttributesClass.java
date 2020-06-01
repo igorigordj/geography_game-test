2b6715c669077049f95001d2c09243dc0ab613ec
igorigordj <egor.gile@gmail.com>
2020-06-01 06:45:07 +0200

    AttributesClass abstract

---
+++ src/pages/AttributesClass.java
@@ -0,0 +1,28 @@
+package pages;
+
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class AttributesClass 
+{
+
+	protected WebDriver driver;
+	protected Properties selectors;
+	protected Properties locators;
+	protected WebDriverWait waiter;
+	
+	public AttributesClass(WebDriver driver, Properties selectors, Properties locators, WebDriverWait waiter) {
+	
+		this.driver = driver;
+		this.selectors = selectors;
+		this.locators = locators;
+		this.waiter = waiter;
+	}
+	
+	
+	
+	
+}
